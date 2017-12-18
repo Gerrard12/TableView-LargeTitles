b45b16c029855357340a45e55204a17c07387944
Gerardo <gsanchezc1211@gmail.com>
2017-12-18 10:01:07 -0500

    first commit

---
+++ DemoInterface/TypeSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TypeSearchTableViewCell.swift
+//  DemoInterface
+//
+//  Created by gerardo on 13/12/17.
+//  Copyright © 2017 Orbis. All rights reserved.
+//
+
+import UIKit
+
+class TypeSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
